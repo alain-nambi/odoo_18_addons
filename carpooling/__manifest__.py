b44e7f43e7b85701ca5cc75f4dc8523cf91cbad8
Alain Nambii <alainnambi@gmail.com>
2024-12-19 11:54:55 +0300

    Odoo 1 : Create __manifest__.py and __init__.py

---
+++ carpooling/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Carpooling Apps',
+    'version': '1.0',
+    'author': 'Alain Nambii',
+    'description': 
+    """
+        Helps you to find a carpooling partner
+    """,
+    'depends': ['base'],
+    'category': 'Custom Tools',
+    'license': 'LGPL-3',
+}
